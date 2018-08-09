288a3513a86c39e7866ebae51687438ac27ad5de
John Seaman <jseaman@metrostarsystems.com>
2018-08-09 08:47:12 -0400

    Added map and integrated data source.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import * as L from 'leaflet';
+import * as esri from 'esri-leaflet';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'site';
+
+  options: L.MapOptions = {
+    layers: [
+      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 14,
+    center: L.latLng(38.957189, -77.352262)
+  };
+
+  onMapReady(map: L.Map) {
+    const getCoordsBar = L.DomUtil.create('div', 'leaflet-bar leaflet-control') as HTMLDivElement;
+    const getCoordsButton = L.DomUtil.create('a', null, getCoordsBar) as HTMLAnchorElement;
+    getCoordsButton.innerText = 'CO';
+    getCoordsButton.setAttribute('role', 'button');
+    getCoordsButton.href = '#';
+    getCoordsButton.onclick = (ev: MouseEvent) => {
+      const bounds = map.getBounds();
+
+      const { lat: south, lng: west } = bounds.getSouthWest();
+      const { lat: north, lng: east } = bounds.getNorthEast();
+
+      esri.query({
+        url: 'http://snap-load-balancer-244858692.us-east-1.elb.amazonaws.com/ArcGIS/rest/services/retailer/MapServer/0'
+      }).within(bounds).run((_, geoJson: GeoJSON.FeatureCollection, fields: any) => {
+        console.log(geoJson);
+        console.log(fields);
+      });
+    };
+
+    let info = new L.Control();
+    info.onAdd = () => getCoordsBar;
+
+    info.addTo(map);
+  }
 }
