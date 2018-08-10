c44c086d44135cab5c0cbe1c62ffc4c1da7599cf
John Seaman <jseaman@metrostarsystems.com>
2018-08-10 09:44:06 -0400

    Added markers to map.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    LeafletModule
+    LeafletModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
