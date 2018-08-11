b86a5c6d1792d12a264671d144dfa06ccb4a0896
John Seaman <jseaman@metrostarsystems.com>
2018-08-11 18:19:18 -0400

    Added locations panel.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 import { AppComponent } from './app.component';
 import { SnapMapComponent } from './snap-map/snap-map.component';
+import { LocationsPanelComponent } from './locations-panel/locations-panel.component';
+import { LocationComponent } from './location/location.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SnapMapComponent
+    SnapMapComponent,
+    LocationsPanelComponent,
+    LocationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/locations-panel/locations-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-locations-panel',
+  templateUrl: './locations-panel.component.html',
+  styleUrls: ['./locations-panel.component.scss']
+})
+export class LocationsPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
