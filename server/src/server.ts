80c36cad31419960cbee10ce706fff2316476737
John Seaman <jseaman@metrostarsystems.com>
2018-08-12 08:11:37 -0400

    Initial commit of server code.

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import express, { Application } from 'express';
+
+import { WelcomeController } from './controllers';
+
+// Create a new express application instance
+const app = express();
+// The port the express app will listen on
+const port = process.env.PORT || 3000;
+
+// Mount the WelcomeController at the /welcome route
+app.use('/welcome', WelcomeController);
+
+// Serve the application at the given port
+app.listen(port, () => {
+  // Success callback
+  console.log(`Listening at http://localhost:${port}/`);
+});
